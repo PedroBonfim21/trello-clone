149036cede77c1b9664cf27954acc75408c3006a
PedroBonfim21 <phenrique646@gmail.com>
2025-06-02 21:29:21 -0300

    create centralized site settings file

---
+++ config/site.ts
@@ -0,0 +1,5 @@
+export const siteConfig = {
+  name: "Taskify",
+  description:
+    "Taskify is a task management app that helps teams collaborate, manage projects, and reach new productivity peaks.",
+};
