e50b2521d109346eb8af15367006a828cdc44409
PedroBonfim21 <phenrique646@gmail.com>
2025-07-10 23:04:23 -0300

    Adds functionality to create boards on the organization page

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,7 +1,30 @@
-const OrganizationIdPage = () => {
+import { Button } from "@/components/ui/button";
+import { create } from "../../../../../actions/create-board";
+import { db } from "@/lib/db";
+import { Board } from "./board";
+
+const OrganizationIdPage = async () => {
+  const boards = await db.board.findMany();
+
   return (
-    <div>
-      Organization Page
+    <div className="flex flex-col space-y-4">
+      <form action={create}>
+        <input
+          id="title"
+          name="title"
+          placeholder="Enter board title"
+          className="border-black border p-1"
+          required
+        />
+        <Button type="submit">
+          Submit
+        </Button>
+      </form>
+      <div className="space-y-2">
+        {boards.map((board)=>(
+          <Board  key={board.id} title={board.title} id={board.id}/>
+        ))}
+      </div>
     </div>
   );
 };
